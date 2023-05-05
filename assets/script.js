1909329c99c7af7b8bea7c66fc49fbb29bc5d08a
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-05-05 15:46:59 +0200

    add: img in script

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    
+    data(){
+        return{
+            welcome: 'Benvenuti su Vue',
+            img:'img/Anonymous_emblem.webp',
+        }
+    }
+});
+
+app.mount('#app');
+
